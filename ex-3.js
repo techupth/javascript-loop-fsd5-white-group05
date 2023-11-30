be205fb35d5beaaaf036c66ee8ae172b14443749
Astrick <mypiarenoo@gmail.com>
2023-12-01 04:05:28 +0700

    has been finished this exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,11 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
 let minNumber;
-// Start coding here
+minNumber = numbers[0]
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] < minNumber) {
+        minNumber = numbers[i]
+    }
+}
 
 console.log(minNumber);
--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,11 @@
 // Exercise #4: Displaying Restaurant Name
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
-// Start coding here
+let i = 0;
+while (i < restaurants.length) {
+    let restaurantString = "Restaurant Name: " + restaurants[i];
+    newRestaurants.push(restaurantString);
+    i++;
+}
 
 console.log(newRestaurants);
